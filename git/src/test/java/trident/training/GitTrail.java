4d8a85ef3db1f8c005279b18fbc9ac50ada6ca17
vigneshvelayutham0507 <vignesh.velayutham@tridentsqa.com>
2025-01-21 19:11:34 +0530

    pull in eclipse
    changed my out put

--- git/src/test/java/trident/training/GitTrail.java
+++ git/src/test/java/trident/training/GitTrail.java
@@ -4,7 +4,7 @@ public class GitTrail {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello git");
+System.out.println("hello git iam going to pull you");
 	}
 
 }
